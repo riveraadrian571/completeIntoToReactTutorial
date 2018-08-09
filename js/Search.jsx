dff4f30701d2373adfa5b952c611bdb492174959
Adrian Rivera <rivera.adrian7465@gmail.com>
2018-08-09 14:22:05 -0400

    added spread operator

--- js/Search.jsx
+++ js/Search.jsx
@@ -7,7 +7,17 @@ const Search = () => (
         { /* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre> */}
         { /* map function will take an array of objects and convert to array of little react components,
         also extracts a property title from each object of the array */ }
-        {preload.shows.map(show => <ShowCard show={show} />)}
+
+        { /*
+        if the object is being passed into props in this fashion then it should be referenced like such:
+        {props.show.title}
+        preload.shows.map(show => <ShowCard show={show} />) */}
+
+        { /* if a spread operator is used (...object) to pass data to props then it should be referenced like such: 
+        {props.title} 
+        providing a key here is important to maintain order in the mapping process 
+        key should be unique here */ }
+        {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
     </div>
 );
 
--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { shape, string } from 'prop-types';
+import { string } from 'prop-types';
 
+/* if props is passed in using an actual object rather than the spread operator (...object) then this is how it
+should be outputted to the screen
 const ShowCard = props => (
     <div className="show-card">
         <div className='show-card'>
@@ -12,9 +14,9 @@ const ShowCard = props => (
             </div>
         </div>
     </div>
-);
-
+); 
 // shape here just means object
+// ***make sure to import shape from prop-types
 // there is a default props-type as well
 ShowCard.propTypes = {
     show: shape({
@@ -24,5 +26,29 @@ ShowCard.propTypes = {
         description: string.isRequired
     }).isRequired
 }
+*/
+
+/*
+if props is displayed using spread operator (...object) then this is how it should be outputted
+*/
+const ShowCard = props => (
+    <div className="show-card">
+        <div className='show-card'>
+            <img alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
+            <div>
+                <h3> {props.title} </h3>
+                <h3> ({props.year}) </h3>
+                <p> {props.description} </p>
+            </div>
+        </div>
+    </div>
+);
+
+ShowCard.propTypes = {
+    poster: string.isRequired,
+    title: string.isRequired,
+    year: string.isRequired,
+    description: string.isRequired
+};
 
 export default ShowCard;
